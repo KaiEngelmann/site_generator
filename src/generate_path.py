088b8ecc8d3bf7b2a7d75022aa3f24c10710a3eb
KaiEngelmann <KaiVaughnMusic@Yahoo.com>
2025-08-17 21:15:45 -0400

    finished project

--- src/generate_path.py
+++ src/generate_path.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r", encoding="utf-8") as f:
         markdown_file = f.read()
@@ -15,11 +15,15 @@ def generate_page(from_path, template_path, dest_path):
     html_string = render_html_node(html_tree)
     title = extract_title(markdown_file)
     filled_template = template_file.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+    if not basepath.endswith("/"):
+        basepath += "/"
+    filled_template = filled_template.replace('href="/', f'href="{basepath}')
+    filled_template = filled_template.replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w", encoding="utf-8") as f:
             f.write(filled_template)
     
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     content_root = Path(dir_path_content)
     template = Path(template_path)
     dest_root = Path(dest_dir_path)
@@ -34,7 +38,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             if os.path.isfile(content_path):
                 if name.endswith(".md"):
                     dest_html_path = str(Path(dest_path).with_suffix(".html"))
-                    generate_page(content_path, str(template), dest_html_path)
+                    generate_page(content_path, str(template), dest_html_path, basepath)
             else:
                 crawl(content_path, dest_path)
     crawl(str(content_root), str(dest_root))
--- src/main.py
+++ src/main.py
@@ -2,25 +2,29 @@ from textnode import TextType, TextNode
 import os
 from copystatic import copy_contents
 from generate_path import generate_pages_recursive
+import sys
 
 def main():
 	
 	node = TextNode("text", TextType.LINK, "somelink.com")
 	formatted = node.text_type.value.format(text=node.text, url=node.url)
 
-	current_script_dir = os.path.dirname(os.path.abspath(__file__))
+	basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+	if not basepath.endswith("/"):
+		basepath += "/"
 
+	current_script_dir = os.path.dirname(os.path.abspath(__file__))
 	project_root = os.path.dirname(current_script_dir)
 
 	static_path = os.path.join(project_root, "static")
-	public_path = os.path.join(project_root, "public")
+	public_path = os.path.join(project_root, "docs")
 	content_index_path = os.path.join(project_root, "content")
 	template = os.path.join(project_root, "template.html")
-	content_destination = os.path.join(public_path)
+	content_destination = public_path
 
 
 	copy_contents(static_path, public_path)
-	generate_pages_recursive(content_index_path, template, content_destination)
+	generate_pages_recursive(content_index_path, template, content_destination, basepath)
 
 
 
